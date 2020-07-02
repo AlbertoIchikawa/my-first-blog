569d57433b500521346f02975701185b0ce56ccd
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-02 16:38:02 +0900

    Vuetify Tutorial #5

--- VuetifyTutorial/todo-ninja/src/views/Home.vue
+++ VuetifyTutorial/todo-ninja/src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="red white--text">PVJNCMJWKRJPCKVHRGUPEUEZRVJWVDMDYYHBVZKPBHACRFFVFHDKPWKXMURJGHDPSNEDNKKCYNVUFTRWLFLZTHUSAJNTHNCJWYNU
-</p>
-<p class="pink lighten-4 red--text text--darken-4">PVJNCMJWKRJPCKVHRGUPEUEZRVJWVDMDYYHBVZKPBHACRFFVFHDKPWKXMURJGHDPSNEDNKKCYNVUFTRWLFLZTHUSAJNTHNCJWYNU
-</p>
-<h1 class="display-4">Massive Display</h1>
-<h4 class="display-1">Smaller Display</h4>
-<p class="headline">This is a headline</p>
-<p class="subhading font-weight-bold">This is a subheading</p>
-<p class="caption">This is a caption</p>
+    <v-btn class="pink white--text">CLICK ME</v-btn>
+    <v-btn depressed color="pink">click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>EMAIl ME</span>
+    </v-btn>
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>EMAIl ME</span>
+    </v-btn>
+    <v-btn depressed large class="pink white--text">
+     <span>EMAIl ME</span> 
+     <v-icon right large >mdi-email</v-icon>
+    </v-btn>
+    <v-btn fab depressed small dark class="purple">
+     <v-icon>mdi-heart</v-icon>
+    </v-btn>
 </div>
 </template>
 
