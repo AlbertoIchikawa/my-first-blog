c4f5cf2f2ee854947fa3c90ee812d04ff3397d32
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-02 16:50:43 +0900

    Vuetify Tutorial #6

--- VuetifyTutorial/todo-ninja/src/views/Home.vue
+++ VuetifyTutorial/todo-ninja/src/views/Home.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <v-btn class="pink white--text">CLICK ME</v-btn>
-    <v-btn depressed color="pink">click me</v-btn>
-    <v-btn text color="pink">click me</v-btn>
+    <!--<v-btn class="hidden-md-and-down">CLICK ME</v-btn>-->
 
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>EMAIl ME</span>
-    </v-btn>
-    <v-btn depressed small class="pink white--text">
-      <v-icon left small>mdi-email</v-icon>
-      <span>EMAIl ME</span>
-    </v-btn>
-    <v-btn depressed large class="pink white--text">
-     <span>EMAIl ME</span> 
-     <v-icon right large >mdi-email</v-icon>
-    </v-btn>
-    <v-btn fab depressed small dark class="purple">
-     <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <!--<v-btn class="hidden-md-and-up">CLICK ME</v-btn>-->
+
+    <v-btn class="hidden-sm-only">CLICK ME</v-btn>
+    
 </div>
 </template>
 
