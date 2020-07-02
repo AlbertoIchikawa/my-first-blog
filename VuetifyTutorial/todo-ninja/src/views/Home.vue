e8a21cb08e4020893a41f2121f2c13643d2d7ff4
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-02 15:41:16 +0900

    Vuetify Tutorial #4

--- VuetifyTutorial/todo-ninja/src/views/Home.vue
+++ VuetifyTutorial/todo-ninja/src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-  </div>
+    <p class="red white--text">PVJNCMJWKRJPCKVHRGUPEUEZRVJWVDMDYYHBVZKPBHACRFFVFHDKPWKXMURJGHDPSNEDNKKCYNVUFTRWLFLZTHUSAJNTHNCJWYNU
+</p>
+<p class="pink lighten-4 red--text text--darken-4">PVJNCMJWKRJPCKVHRGUPEUEZRVJWVDMDYYHBVZKPBHACRFFVFHDKPWKXMURJGHDPSNEDNKKCYNVUFTRWLFLZTHUSAJNTHNCJWYNU
+</p>
+<h1 class="display-4">Massive Display</h1>
+<h4 class="display-1">Smaller Display</h4>
+<p class="headline">This is a headline</p>
+<p class="subhading font-weight-bold">This is a subheading</p>
+<p class="caption">This is a caption</p>
+</div>
 </template>
 
 <script>
