e7fa24cecd0d85121bdda3c89456a82a4707aa2a
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-03 16:24:04 +0900

    day3課題１のアラート表示を追加しました。

---
+++ VuetifyTutorial/kadai/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-btn large @click="greet" color="red">PUSH</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    
+  }),
+  methods: {
+      greet: function() {
+        // メソッド内の `this` は、 Vue インスタンスを参照します
+        alert("Alert表示課題 ");
+        // `event` は、ネイティブ DOM イベントです
+      }
+    }
+
+};
+</script>
