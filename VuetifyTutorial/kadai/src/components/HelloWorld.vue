abe76a7602bb4d665386b2054cb4e22b6ca38af0
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-06 18:19:13 +0900

    day4_課題１のToggl機能を付けました。

--- VuetifyTutorial/kadai/src/components/HelloWorld.vue
+++ VuetifyTutorial/kadai/src/components/HelloWorld.vue
@@ -1,23 +1,26 @@
 <template>
-  <v-container>
-    <v-btn large @click="greet" color="red">PUSH</v-btn>
-  </v-container>
+  <div class="hello">
+    <h1 class="text-center">{{ msg }}</h1>
+    <h2 v-show="showText" class="text-center">{{ text }}</h2>
+    <div align="center"><v-btn   rounded color="#008000"  @click="toggle">toggle</v-btn> <!--追加!!!!!!--></div>
+    
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-
-  data: () => ({
-    
-  }),
-  methods: {
-      greet: function() {
-        // メソッド内の `this` は、 Vue インスタンスを参照します
-        alert("Alert表示課題 ");
-        // `event` は、ネイティブ DOM イベントです
-      }
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '課題１のトグル機能',
+      text: 'Test data',
+      showText: false
     }
-
-};
+  },//こっから下追加！！！！！！！！
+  methods: { 
+    toggle: function () {
+      this.showText = !this.showText
+    }
+  }
+}
 </script>
