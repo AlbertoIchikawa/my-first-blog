3091206a01ba7101fe6a8c15b9cf498de4c4081a
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-06 17:44:50 +0900

    Vuetify_Tutorial#16

--- VuetifyTutorial/todo-ninja/src/views/Dashboard.vue
+++ VuetifyTutorial/todo-ninja/src/views/Dashboard.vue
@@ -3,31 +3,33 @@
     <v-subheader class="grey--text">Dashboard</v-subheader>
 
     <v-container class="my-5">
-
-       <v-card flat tile class="px-3" v-for="project in projects" :key="project.title">
-   <v-row row wrap>
-     <v-col cols="12" md="6" :class="`pl-3 project ${project.status}`">
-       <div class="caption grey--text">Project Title</div>
-       <div>{{project.title}}</div>
-       </v-col>
-       <v-col xs="2">
-         <div class="caption grey--text">Person</div>
-       <div>{{project.person}}</div>
-       </v-col>
+      <v-card flat tile class="px-3" v-for="project in projects" :key="project.title">
+        <v-row row wrap>
+          <v-col cols="12" md="6" :class="`pl-3 project ${project.status}`">
+            <div class="caption grey--text">Project Title</div>
+            <div>{{project.title}}</div>
+          </v-col>
           <v-col xs="2">
-         <div class="caption grey--text">Due Date</div>
-       <div>{{project.due}}</div>
-       </v-col>
-        <v-col xs="2">
-         <div class="caption grey--text">Status</div>
-       <div>{{project.status}}</div>
-       </v-col>
-  </v-row>
-  <v-row cols="12">
-<v-divider></v-divider>
-</v-row>
-</v-card>
-
+            <div class="caption grey--text">Person</div>
+            <div>{{project.person}}</div>
+          </v-col>
+          <v-col xs="2">
+            <div class="caption grey--text">Due Date</div>
+            <div>{{project.due}}</div>
+          </v-col>
+          <v-col xs="2">
+            <div class="right">
+              <v-chip
+                small
+                :class="`${project.status} white--text caption my-2`"
+              >{{ project.status }}</v-chip>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row cols="12">
+          <v-divider></v-divider>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -36,29 +38,64 @@
 // @ is an alias to /src
 
 export default {
-  data () {
+  data() {
     return {
       projects: [
-        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'}
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
       ]
-    }
+    };
   }
 };
 </script>
 
 <style>
-
-.project.complete{
+.project.complete {
   border-left: 4px solid #3cd1c2;
 }
-.project.ongoing{
+.project.ongoing {
   border-left: 4px solid orange;
 }
-.project.overdue{
+.project.overdue {
   border-left: 4px solid tomato;
 }
-
+.v-chip.complete{
+  background:#3cd1c2!important;
+}
+.v-chip.ongoing{
+  background:#ffaa2c!important;
+}
+.v-chip.overdue{
+  background:#f83e70!important;
+}
 </style>
