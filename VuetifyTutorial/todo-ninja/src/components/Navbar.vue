746e3c4da446148d89562f6eb6b692ae6195b210
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-07 13:37:58 +0900

    Vuetify_Tutorial#20

--- VuetifyTutorial/todo-ninja/src/components/Navbar.vue
+++ VuetifyTutorial/todo-ninja/src/components/Navbar.vue
@@ -14,6 +14,16 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" temporary class="primary">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/icon1.jpg">
+          </v-avatar>
+          <p class="white--text subheading mt-1">
+            The Net Ninja
+          </p>
+        </v-flex>
+      </v-layout>
       <v-list>
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
--- VuetifyTutorial/todo-ninja/src/views/Team.vue
+++ VuetifyTutorial/todo-ninja/src/views/Team.vue
@@ -7,7 +7,9 @@
         <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
           <v-card class="text-center ma-3">
             <v-responsive class="pt-4">
-              image goes here
+              <v-avatar size="100" class="grey lighten-2">
+                <img :src="person.avatar">
+              </v-avatar>
             </v-responsive>
             <v-card-text>
               <div class="subheading">{{ person.name }}</div>
@@ -33,11 +35,11 @@ export default {
   data() {
     return {
       team: [
-        { name: "The Net Ninja", role: "Web developer" },
-        { name: "Ryu", role: "Graphic designer" },
-        { name: "Chun Li", role: "Web developer" },
-        { name: "Gouken", role: "Social media maverick" },
-        { name: "Yoshi", role: "Sales guru" }
+        { name: "The Net Ninja", role: "Web developer" , avatar: '/icon1.jpg'},
+        { name: "Ryu", role: "Graphic designer" , avatar: '/icon2.jpg'},
+        { name: "Chun Li", role: "Web developer" , avatar: '/icon3.jpg'},
+        { name: "Gouken", role: "Social media maverick" , avatar: '/icon4.jpg'},
+        { name: "Yoshi", role: "Sales guru" , avatar: '/icon5.jpg'}
       ]
     };
   }
