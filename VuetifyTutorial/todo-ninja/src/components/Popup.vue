db7ff65078fcf7f7f020227857a46c79f9899d53
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-07 18:24:20 +0900

    Vuetify_Tutorial#24

--- VuetifyTutorial/todo-ninja/src/components/Popup.vue
+++ VuetifyTutorial/todo-ninja/src/components/Popup.vue
@@ -1,19 +1,35 @@
 <template>
-    <v-dialog max-width="600px">
-        <template v-slot:activator="{on}">
-
-        <v-btn  v-on="on" text slot="activator" class="success">Add new project</v-btn>
-        </template>
-        <v-card>
-            <v-card-title>
-                <h2>Add a New Project</h2>
-            </v-card-title>
-        </v-card>
-    </v-dialog>
+  <v-dialog max-width="600px">
+    <template v-slot:activator="{on}">
+      <v-btn v-on="on" text slot="activator" class="success">Add new project</v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <h2>Add a New Project</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+        <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      title: "",
+      content: ""
+    }
+  },
+  methods:{
+      submit(){
+          console.log(this.title,this.content)
+      }
+  }
+};
 </script>
