8935f5c854ac64819d997cb5d1de0f584ca8d88b
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-08 16:29:21 +0900

    day5_課題１

--- VuetifyTutorial/kadai/src/components/HelloWorld.vue
+++ VuetifyTutorial/kadai/src/components/HelloWorld.vue
@@ -1,51 +1,104 @@
 <template>
-  <v-card class="mx-auto" max-width="450">
-    <!-- src：画像を読み込んでいる。 -->
-    <!-- aspect-ratioで比率を調整 -->
-    <v-avatar>
-    <v-img src="https://cdn.vuetifyjs.com/images/cards/house.jpg" :aspect-ratio="16/9"></v-img>
-    </v-avatar>
-    <v-card-title>
-      <div class="display-1 mb-2">Welcome Home...</div>
-      <div class="title font-weight-regular grey--text">Monday, 12:30 PM, Mostly Sunny</div>
-    </v-card-title>
-    <v-row class="px-4 grey--text" align="center">
-      <v-avatar size="24" class="mr-4">
-        <!-- 天気マークの画像を読み込み -->
-        <v-img src="https://cdn.vuetifyjs.com/images/weather/part-cloud-48px.png" contain></v-img>
-      </v-avatar>
-      <span>81° / 62°</span>
-    </v-row>
-    <!-- 罫線を表示 -->
-    <v-divider class="mt-6 mx-4"></v-divider>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-expansion-panels popout>
+        <!-- 拡張for文みたいなもの(project, i) -->
+        <v-expansion-panel v-for="(project, i) in projects" :key="i" hide-actions>
+          <v-expansion-panel-header>
+            <v-row align="center" class="spacer" no-gutters>
+              <!-- this v-col is for avatar and name or title -->
+              <v-col cols="4" sm="2" md="1">
+                <v-avatar size="36">
+                  <!-- v-ifでavatarの場合 -->
+                  <img v-if="project.avatar" :src="project.avatar" />
+                  <!-- v-elseでそれ以外の場合。今回だとアイコンを表示 -->
+                  <v-icon
+                    v-else
+                    :color="project.color"
+                    v-text="project.icon"
+                  ></v-icon>
+                </v-avatar>
+              </v-col>
 
-    <!-- ３つのToggl機能を配置 -->
-    <v-card-text>
-      <v-chip class="mr-2" @click="lights">
-        <v-icon left>mdi-brightness-5</v-icon>Turn on Lights
-      </v-chip>
-      <v-chip class="mr-2" @click="alarm">
-        <v-icon left>mdi-alarm-check</v-icon>Set alarm
-      </v-chip>
-      <v-chip @click="blinds">
-        <v-icon left>mdi-blinds</v-icon>Close blinds
-      </v-chip>
-    </v-card-text>
-  </v-card>
+              <!-- showing Name and title -->
+              <v-col class="hidden-xs-only" sm="5" md="3">
+                <strong v-html="project.name"></strong>
+              <span
+                  v-if="project.total"
+                  class="grey--text"
+                >
+                <!-- &nbsp stands non-break-space -->
+                  &nbsp;({{ project.total }})
+              </span>
+              </v-col>
+
+
+              <!-- this v-col is for Main title -->
+              <v-col class="text-no-wrap" cols="5" sm="3">
+                <!-- showing new icon with using v-ship -->
+                <v-chip
+                  v-if="project.new"
+                  :color="`${project.color} lighten-4`"
+                  class="ml-0 mr-2 black--text"
+                  label
+                  small
+                >
+                  {{ project.new }} new
+                </v-chip>
+                <strong v-html="project.head"></strong>
+              </v-col>
+              
+              <!-- this v-col is for subtitle like grey-message -->
+              <v-col
+                v-if="project.excerpt"
+                class="grey--text text-truncate hidden-sm-and-down"
+              > &mdash; {{ project.excerpt }}</v-col>
+            </v-row>
+          </v-expansion-panel-header>
+          <!-- パネル内のコンテンツ。 -->
+          <v-expansion-panel-content>
+            <!-- v-dividerで区切り線を入れている。 -->
+            <v-divider></v-divider>
+            <!-- script内のcontentsを引き出す。 -->
+            <v-card-text v-text="contents"></v-card-text>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  methods: {
-    alarm() {
-      alert("Turning on alarm...");
-    },
-    blinds() {
-      alert("Toggling Blinds...");
-    },
-    lights() {
-      alert("Toggling lights...");
-    }
+  data() {
+    return {
+      projects: [
+        {
+          avatar: "/img1.png",
+          name: "John Leider",
+          head: "Welcome to Vuetify.js!",
+          excerpt: "Thank you for joining our community..."
+        },
+        {
+          icon: "people_alt",
+          color:"red",
+          name: "Social",
+          new:1,
+          total:"3",
+          head: "Twitter"
+        },
+        {
+          icon: "local_offer",
+          color:"teal",
+          name: "Promos",
+          new:2,
+          total:"4",
+          head: "Shop your way",
+        }
+      ],
+      contents:
+        "Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum."
+    };
   }
 };
 </script>
