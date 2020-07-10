e02f4693d52e84f23673c9bf62f76d7ec74cff65
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-10 11:53:54 +0900

    リストからスライスするソースコード例

--- HeadFirstPython/ch01/vowels.py
+++ HeadFirstPython/ch01/vowels.py
@@ -1,23 +1,25 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-word = input("単語を入力してください。母音を探します。")
-found = []
-for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found.append(letter)
-for vowels in found:
-    print(vowels)
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# word = input("単語を入力してください。母音を探します。")
+# found = []
+# for letter in word:
+#     if letter in vowels:
+#         if letter not in found:
+#             found.append(letter)
+# for vowels in found:
+#     print(vowels)
 
 phrase = "Don't panic!"
 plist = list (phrase)
 print(phrase)
 print(plist)
-for i in range(4):
-    plist.pop()
-plist.pop(0)
-plist.remove("'")
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
-new_phrase = ''.join(plist)
+new_phrase = ''.join(plist[1:3])
+new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
+# for i in range(4):
+#     plist.pop()
+# plist.pop(0)
+# plist.remove("'")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+# new_phrase = ''.join(plist)
 print(plist)
 print(new_phrase)
