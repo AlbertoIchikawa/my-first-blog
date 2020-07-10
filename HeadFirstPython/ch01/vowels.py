1d339aec828fa60e3268e6ea1204b2350aae4ad6
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-10 18:06:49 +0900

    HeadFirstPthon_p131_intersectionのソースコード例

--- HeadFirstPython/ch01/vowels.py
+++ HeadFirstPython/ch01/vowels.py
@@ -1,9 +1,5 @@
-vowels = ["a","e","i","o","u"]
-word = input("単語を入力してください。母音を探します")
-found = {}
-for letters in word:
-    if letters in vowels:
-        found.setdefault(letters, 0)
-        found[letters] += 1
-for k, v in sorted(found.items()):
-    print(k, 'の出現回数は', v, '回です。')
+vowels = set('aeiou')
+word = input('単語を入力してください。母音を探します。')
+found = vowels.intersection(set(word))
+for vowels in found:
+    print(vowels)
