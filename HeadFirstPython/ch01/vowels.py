e30cfbf016a6b8d1d7e708b411ffebcbebc5aee5
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-10 17:54:44 +0900

    HeadFirstPython_p120_頻度マグネットとsetdefaultメソッドを使うソースコード例

--- HeadFirstPython/ch01/vowels.py
+++ HeadFirstPython/ch01/vowels.py
@@ -1,26 +1,9 @@
-# vowels = ['a', 'e', 'i', 'o', 'u']
-# word = input("単語を入力してください。母音を探します。")
-# found = []
-# for letter in word:
-#     if letter in vowels:
-#         if letter not in found:
-#             found.append(letter)
-# for vowels in found:
-#     print(vowels)
-
-phrase = "Don't panic!"
-plist = list (phrase)
-print(phrase)
-print(plist)
-new_phrase = ''.join(plist[1:3])
-new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
-# for i in range(4):
-#     plist.pop()
-# plist.pop(0)
-# plist.remove("'")
-# plist.extend([plist.pop(), plist.pop()])
-# plist.insert(2, plist.pop(3))
-# new_phrase = ''.join(plist)
-print(plist)
-print(new_phrase)
-
+vowels = ["a","e","i","o","u"]
+word = input("単語を入力してください。母音を探します")
+found = {}
+for letters in word:
+    if letters in vowels:
+        found.setdefault(letters, 0)
+        found[letters] += 1
+for k, v in sorted(found.items()):
+    print(k, 'の出現回数は', v, '回です。')
