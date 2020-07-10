b197d79ae682d5277d08ba895f289ff0504b2621
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-10 11:13:22 +0900

    List_remove_pop_extend_insertを使ったソースコード

--- HeadFirstPython/ch01/vowels.py
+++ HeadFirstPython/ch01/vowels.py
@@ -7,3 +7,17 @@ for letter in word:
             found.append(letter)
 for vowels in found:
     print(vowels)
+
+phrase = "Don't panic!"
+plist = list (phrase)
+print(phrase)
+print(plist)
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
