5cba0e0bbf5e35a0aa089dc13ca5d863244f9eb7
POL-18016\市川航 <ichikawa@pol-japan.co.jp>
2020-07-13 17:13:15 +0900

    day8_課題４_sortArray

---
+++ HeadFirstPython/string_splosion problem/sort_array.py
@@ -0,0 +1,16 @@
+def sort_array(self, nums):
+
+
+
+def sort_array_test():
+    print("sort_array_test: begin")
+    assert sort_array([]) == [],"FT1"
+    assert sort_array([1]) == [1]
+    assert sort_array([1, 1]) == [1]
+    assert sort_array([1, 2]) == [1, 2]
+    assert sort_array([1, 2, 2]) == [1, 2]
+    assert sort_array([1, 2, 3]) == [1, 2, 3]
+    print("sort_array_test: done")
+
+
+sort_array_test()
