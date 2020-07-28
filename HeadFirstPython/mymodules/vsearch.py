d44e9d48d6ad5efa1dc89f972790418b9e56d21b
AlbertoIchikawa <ichikawa@pol-japan.co.jp>
2020-07-28 18:22:09 +0900

    no message

--- HeadFirstPython/mymodules/vsearch.py
+++
@@ -1,9 +0,0 @@
-def search4vowels(phrase: str) -> set:
-    """母音が見つかったかどうかによってブール値を返す。"""
-    vowels = set('aeiou')
-    return vowels.intersection(set(phrase))
-
-
-def search4letters(phrase: str, letters: str = 'aeiou') -> set:
-    """phrase内のlettersの集合を返す。"""
-    return set(letters).intersection(set(phrase))
